1552d8193d06cd36b53f871eadf3c9d2e520e190
Henning Koch <henning.koch@makandra.de>
2015-10-30 16:01:09 +0100

    Allow railslts-version.rb to be required multiple times

--- railslts-version/lib/railslts-version.rb
+++ railslts-version/lib/railslts-version.rb
@@ -1,9 +1,19 @@
 module RailsLts
   module VERSION
-    MAJOR = 2
-    MINOR = 3
-    TINY = 18
-    LTS = 14
-    STRING = [MAJOR, MINOR, TINY, LTS].join('.')
+    # Because of the we way we're requiring this files in .gemspecs,
+    # it might be loaded multiple times when installing over Git.
+    unless defined?(STRING)
+
+      MAJOR = 2
+      MINOR = 3
+      TINY = 18
+      LTS = 14
+      STRING = [MAJOR, MINOR, TINY, LTS].join('.')
+
+      def self.to_s
+        STRING
+      end
+
+    end
   end
 end
