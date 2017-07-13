6f8d5a560e6658c5f89e960d42ef3549bbc7dc01
Tobias Kraze <tobias.kraze@makandra.de>
2017-07-13 17:20:41 +0200

    railties: generator template renderer compatibility for Ruby 2+

--- railties/lib/rails_generator/commands.rb
+++ railties/lib/rails_generator/commands.rb
@@ -299,7 +299,13 @@ HELP
             # Evaluate any assignments in a temporary, throwaway binding.
             vars = template_options[:assigns] || {}
             b = template_options[:binding] || binding
-            vars.each { |k,v| eval "#{k} = vars[:#{k}] || vars['#{k}']", b }
+            if b.respond_to?(:local_variable_set)
+              vars.each do |k,v|
+                b.local_variable_set(:"#{k}", v)
+              end
+            else
+              vars.each { |k,v| eval "#{k} = vars[:#{k}] || vars['#{k}']", b }
+            end
 
             # Render the source file with the temporary binding.
             ERB.new(file.read, nil, '-').result(b)
