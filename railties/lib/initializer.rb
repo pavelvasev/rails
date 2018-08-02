f5035938b055ff542824fccd277ac40ad69d1e00
Tobias Kraze <tobias.kraze@makandra.de>
2018-08-02 09:39:18 +0200

    fix "rails new" after changes to gem handling

--- railties/lib/initializer.rb
+++ railties/lib/initializer.rb
@@ -2,23 +2,7 @@ require 'logger'
 require 'set'
 require 'pathname'
 
-module Rails
-  class << self
-    if defined?(LTS_ENABLE_GEM_HANDLING)
-      def enable_gem_handling?
-        LTS_ENABLE_GEM_HANDLING
-      end
-    elsif defined?(Gem::VERSION) && Gem::VERSION >= '2'
-      def enable_gem_handling?
-        false
-      end
-    else
-      def enable_gem_handling?
-        true
-      end
-    end
-  end
-end
+require 'lts_gem_handling'
 
 $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'railties_path'
