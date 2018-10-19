bc24c1254079d292c31feb0f975a27f9c0d4e1ea
Tobias Kraze <tobias.kraze@makandra.de>
2018-10-19 19:13:25 +0200

    2.5 compat; actionpack: Fixnum deprecations

--- actionpack/lib/action_view/helpers/date_helper.rb
+++ actionpack/lib/action_view/helpers/date_helper.rb
@@ -735,7 +735,7 @@ module ActionView
       private
         %w( sec min hour day month year ).each do |method|
           define_method(method) do
-            @datetime.kind_of?(Fixnum) ? @datetime : @datetime.send(method) if @datetime
+            @datetime.kind_of?(ActiveSupport::IntegerClass) ? @datetime : @datetime.send(method) if @datetime
           end
         end
 
